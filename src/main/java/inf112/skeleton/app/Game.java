45a70c2b65994b99d851541df01082c9d2631ba2
Isak Hølleland <isak.holleland@student.uib.no>
2021-02-10 13:54:36 +0100

    [Oblig1] Refactored (changed name): Main.class -> Launcher.class HelloWorld.class -> Game.class

--- src/main/java/inf112/skeleton/app/Game.java
+++ src/main/java/inf112/skeleton/app/Game.java
@@ -21,7 +21,7 @@ import com.badlogic.gdx.math.Vector2;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HelloWorld extends InputAdapter implements ApplicationListener {
+public class Game extends InputAdapter implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
     private TiledMap mapTile;
