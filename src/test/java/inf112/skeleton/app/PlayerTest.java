97008ba230c451546a6d182eb1eb6068f2d44780
Isak Hølleland <isak.holleland@student.uib.no>
2021-02-11 11:41:36 +0100

    [Oblig1] Cleaned up the PlayerTest

--- src/test/java/inf112/skeleton/app/PlayerTest.java
+++ src/test/java/inf112/skeleton/app/PlayerTest.java
@@ -4,42 +4,51 @@ import static org.junit.Assert.*;
 import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.player.LocalPlayer;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 
 public class PlayerTest {
+    LocalPlayer player;
+    Vector2 playerPosition;
+
+
+    @BeforeEach
+    public void setup(){
+        player = new LocalPlayer();
+        playerPosition = new Vector2(2,1);
+    }
+
     @Test
     public void playerSetPositionIsGetPosition1(){
-        LocalPlayer player = new LocalPlayer();
-        Vector2 playerPosition = new Vector2(1,1);
+        playerPosition = new Vector2(1,1);
         player.setPosition(playerPosition);
         assertEquals(new Vector2(1,1), player.getPosition());
     }
     @Test
     public void playerSetPositionIsGetPosition2(){
-        LocalPlayer player = new LocalPlayer();
-        Vector2 playerPosition = new Vector2(2,1);
         player.setPosition(playerPosition);
         assertEquals(new Vector2(2,1), player.getPosition());
     }
     @Test
     public void playerFaceNorthAndGoesOneStepForwardAndThenOneBackward(){
-        LocalPlayer player = new LocalPlayer();
-        player.setPosition(new Vector2(2,1));
         player.setFacingDirection(Direction.NORTH);
+
         player.move(Action.FORWARD);
         assertEquals(new Vector2(2,2), player.getPosition());
+
         player.move(Action.REVERSE);
         assertEquals(new Vector2(2,1), player.getPosition());
     }
     @Test
     public void playerTurnsLeftThenRightAndLastUturn(){
-        LocalPlayer player = new LocalPlayer();
-        player.setPosition(new Vector2(2,1));
         player.setFacingDirection(Direction.NORTH);
+
         player.move(Action.ROTATE_LEFT);
         assertEquals(Direction.WEST, player.getFacingDirection());
+
         player.move(Action.ROTATE_RIGHT);
         assertEquals(Direction.NORTH, player.getFacingDirection());
+
         player.move(Action.U_TURN);
         assertEquals(Direction.SOUTH, player.getFacingDirection());
 
