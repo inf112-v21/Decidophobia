b80ddededb97921681c6dbe58519d81bc13cbf14
Cherzty <cherzty@gmail.com>
2021-03-13 16:50:55 +0100

    einaste knappen som ska teoretisk fungere e exit knappen at the moment

--- src/main/java/inf112/skeleton/app/screen/MainMenuScreen.java
+++ src/main/java/inf112/skeleton/app/screen/MainMenuScreen.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -72,7 +71,7 @@ public class MainMenuScreen implements Screen {
         //Quits the game
         exitButton.addListener(new ClickListener() {
 
-           public void cliked(InputEvent event, float x, float y) {
+           public void clicked(InputEvent event, float x, float y) {
                Gdx.app.exit();
            }
         });
