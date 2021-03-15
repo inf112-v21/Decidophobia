6d936b3d9ea4a3a4bc24d62cfe5c498dd9d20f73
Alexander Gullestad <alexander.gullestad@gmail.com>
2021-03-15 10:08:03 +0100

    Updated LobbyInfo and PlayerInfo - LobbyInfo contains List of all playerInfo and list of players ready - Added playerNr to PlayerInfo

--- src/main/java/inf112/skeleton/app/Multiplayer/packets/LobbyInfo.java
+++ src/main/java/inf112/skeleton/app/Multiplayer/packets/LobbyInfo.java
@@ -1,4 +1,31 @@
 package inf112.skeleton.app.Multiplayer.packets;
 
+import java.util.List;
+import java.util.Map;
+
 public class LobbyInfo {
+    private List<PlayerInfo> players;
+    private Map<Integer, Boolean> playersReady;
+
+    public LobbyInfo(List<PlayerInfo> players, Map<Integer, Boolean> playersReady) {
+        this.players = players;
+        this.playersReady = playersReady;
+    }
+
+    public List<PlayerInfo> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<PlayerInfo> players) {
+        this.players = players;
+    }
+
+    public Map<Integer, Boolean> getPlayersReady() {
+        return playersReady;
+    }
+
+    public void setPlayersReady(Map<Integer, Boolean> playersReady) {
+        this.playersReady = playersReady;
+    }
+
 }
--- src/main/java/inf112/skeleton/app/Multiplayer/packets/PlayerInfo.java
+++ src/main/java/inf112/skeleton/app/Multiplayer/packets/PlayerInfo.java
@@ -1,14 +1,24 @@
 package inf112.skeleton.app.Multiplayer.packets;
 
 public class PlayerInfo {
+    private int playerNr;
     private String nickname;
     private int robotReferenceNr;
     private Boolean isHost;
 
     public PlayerInfo(int playerNr, Boolean isHost) {
+        this.playerNr = playerNr;
         nickname = "P"+playerNr+1;
         robotReferenceNr = playerNr;
         this.isHost = isHost;
+    }
+
+    public int getPlayerNr() {
+        return playerNr;
+    }
+
+    public void setPlayerNr(int playerNr) {
+        this.playerNr = playerNr;
     }
 
     public String getNickname() {
