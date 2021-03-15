aed5f61fbc8a7d5a48221477cb11725200a61745
Alexander Gullestad <alexander.gullestad@gmail.com>
2021-03-15 09:50:37 +0100

    Updated PlayerInfo PlayerInfo contains nickname, robotReferenceNr and if player is host

--- src/main/java/inf112/skeleton/app/Multiplayer/packets/PlayerInfo.java
+++ src/main/java/inf112/skeleton/app/Multiplayer/packets/PlayerInfo.java
@@ -1,4 +1,37 @@
 package inf112.skeleton.app.Multiplayer.packets;
 
 public class PlayerInfo {
+    private String nickname;
+    private int robotReferenceNr;
+    private Boolean isHost;
+
+    public PlayerInfo(int playerNr, Boolean isHost) {
+        nickname = "P"+playerNr+1;
+        robotReferenceNr = playerNr;
+        this.isHost = isHost;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getRobotReferenceNr() {
+        return robotReferenceNr;
+    }
+
+    public void setRobotReferenceNr(int robotReferenceNr) {
+        this.robotReferenceNr = robotReferenceNr;
+    }
+
+    public Boolean getHost() {
+        return isHost;
+    }
+
+    public void setHost(Boolean host) {
+        isHost = host;
+    }
 }
