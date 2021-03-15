1efcc150ac5ac0c125babef1881e76b589354445
Alexander Gullestad <alexander.gullestad@gmail.com>
2021-03-15 11:37:04 +0100

    Updated GameCards GameCards now contains allPlayerHands and Deck

--- src/main/java/inf112/skeleton/app/Multiplayer/packets/GameCards.java
+++ src/main/java/inf112/skeleton/app/Multiplayer/packets/GameCards.java
@@ -1,4 +1,33 @@
 package inf112.skeleton.app.Multiplayer.packets;
 
+import inf112.skeleton.app.cards.Deck;
+import inf112.skeleton.app.cards.PlayerCards;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameCards {
+    private Map<Integer, PlayerCards> allPlayerHands;
+    private Deck deck;
+
+    public GameCards(Deck deck) {
+        this.allPlayerHands = new HashMap<Integer, PlayerCards>();
+        this.deck = deck;
+    }
+
+    public Map<Integer, PlayerCards> getAllPlayerHands() {
+        return allPlayerHands;
+    }
+
+    public void setAllPlayerHands(Map<Integer, PlayerCards> allPlayerHands) {
+        this.allPlayerHands = allPlayerHands;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
 }
